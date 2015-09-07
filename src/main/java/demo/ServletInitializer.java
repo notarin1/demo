002276c58ffab3e98b2e1d1e5542fd3eb54aed0d
Keisuke Inoue <notarin1jp@gmail.com>
2015-09-08 02:27:05 +0900

    コメント追加

--- src/main/java/demo/ServletInitializer.java
+++ src/main/java/demo/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// コメント
 		return application.sources(DemoApplication.class);
 	}
 
